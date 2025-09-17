f601b6559859e2cc94579d252cd93fccb4c0cb5c
ak <ak.personalprojects@gmail.com>
2025-09-17 09:24:29 +0300

    commenting code

--- 01_Python_Automation/01_Files_Management_and_Automation/02_Write_File/starter.py
+++ 01_Python_Automation/01_Files_Management_and_Automation/02_Write_File/starter.py
@@ -1,5 +1,5 @@
 # Exercise 2 — Write to File
 
 # TODO:
-with open('greeting.txt', 'w', encoding='utf-8') as f:
- f.write('Welcome to File Management!\n')
+# with open('greeting.txt', 'w', encoding='utf-8') as f:
+#  f.write('Welcome to File Management!\n')
